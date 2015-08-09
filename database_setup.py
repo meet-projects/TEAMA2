01cc6ee22165f5421862a277133e8420812e0096
Kiosk <kiosk@cs.huji.ac.il>
2015-08-09 12:45:23 +0300

    data tables

--- database_setup.py
+++ database_setup.py
@@ -7,3 +7,29 @@ Base = declarative_base()
 
 #PLACE YOUR TABLE SETUP INFORMATION HERE
 
+class User(Base):
+	_tablename_= 'user'
+	id = Column(Integer,primary_key=True)
+	name = Column(String(60))
+	username = Column(String(60))
+	password = Column(String(60))
+	email = Column(String(60))
+	birthday = Column(Integer)
+	birthmonth = Column(String(30))
+	birthyear = Column(Integer)
+
+class Post(Base):
+        _tablename_='post'
+        id = Column(Integer,primary_key=True)
+        group_id = Column(Integer, ForeignKey('group.id'))
+        group= relationship(Group)
+        user_poster_id = Column(Integer, ForeignKey('user.id'))
+        user = relationship(User)
+        content = Column(String(100))
+        
+
+class Group(Base):
+        _tablename_= 'group'
+        id = Column(Integer,primary_key=True)
+        name = Column(String(50))
+        picture = Column(String)
