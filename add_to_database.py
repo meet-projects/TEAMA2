827692ddaa66dce148a0baed2b45e2e31a75706d
Kiosk <kiosk@cs.huji.ac.il>
2015-08-10 15:36:32 +0300

    sdfh

--- add_to_database.py
+++ add_to_database.py
@@ -10,10 +10,9 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-group=Group(name="hugafriend")
-session.add(group)
-session.commit()
 
-user=User(name="Hen",username="HenG",password="HenG",email="hennygrr28@gmail.com")
-session.add(user)
+
+
+group2=Group(name='group 2')
+session.add(group2)
 session.commit()
